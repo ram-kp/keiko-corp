ab7d85e87e25ecfcaa9b5a132799cf0e6dc10e4e
ram-kp <63231427+ram-kp@users.noreply.github.com>
2020-08-24 11:47:53 +0530

    maybe final commit

--- js/main.js
+++ js/main.js
@@ -5,38 +5,19 @@ $(document).ready(function(){
 		event.preventDefault();
 		var section = $(this).attr('href');
 		var section_pos = $(section).position();
-
 		if(section_pos){
 			$(window).scrollTo({top:section_pos.top, left:'0px'}, 1000);
 		}
-		
 	});
-
 	$('.app_link').click(function(e){
 		event.preventDefault();
 		$(window).scrollTo({top:$("#hero").position().top, left:'0px'}, 1000);		
 	});
-
-
-
-
-
-
-
-
 	//Show & Hide menu on mobile
 	$('.burger_icon').click(function(){
 		$('header nav').toggleClass('show');
 		$('header .burger_icon').toggleClass('active');
 	});
-
-	
-
-
-
-
-
-
 	//wow.js on scroll animations initialization
 	wow = new WOW(
 	    {
@@ -46,24 +27,8 @@ $(document).ready(function(){
 		}
 	);
 	wow.init();
-
-
-
-
-
-
-
-
 	//parallax effect initialization
 	$('.hero').parallax("50%", 0.3);
-
-
-
-
-
-
-
-
 	//Nice scroll initialization
 	$("html").niceScroll({
 		scrollspeed: 50,
@@ -75,14 +40,6 @@ $(document).ready(function(){
 		cursorcolor : '#1f1f1f',
 		zindex : 999
 	});
-
-
-
-
-
-
-
-
 	//Testimonials slider initialization
 	$("#tslider").owlCarousel({
 		items : 1,
@@ -95,26 +52,17 @@ $(document).ready(function(){
 		autoPlay : true,
 		transitionStyle : "fade"
 	});
-
-
-
-
-
-
-
 	//Mailchimp subscription form initialization
 	$('#submit_form').submit(function(){
 		$('#mc_submit').attr('disabled', 'disabled');		
    		processing('icon', 'loading');
 	});
-
 	if($('#submit_form').length){
 		//Mailchim Subscription form
 		$('#submit_form').ajaxChimp({
 		    callback: chimpResponce
 		});
 	}	
-
 	//Mail chimp callback function
 	function chimpResponce(resp) {
    		if (resp.result === 'success') {   			
@@ -130,38 +78,20 @@ $(document).ready(function(){
 	    	$('#mc_submit').removeAttr('disabled', 'disabled');
 	    }
 	}
-
 	function processing(hide, show){
 			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
-
-
-
-
-
-
-
-
 	//Popup video
 	$('#play_video').click(function(e){
 		e.preventDefault();	
-
 		var video_link = $(this).data('video');
 		video_link = '<iframe src="' + video_link + '" width="500" height="208" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
 		$('.about_video').append(video_link).fadeIn(200);
 	});
-
 	$('.close_video').click(function(e){
 		e.preventDefault();	
-
 		$('.about_video').fadeOut(200,function(){
 			$('iframe', this).remove();
 		});
-
 	});
-
-
-
-
 });
